1b4e04e7e693e691f1f7d511779744d1d0b07f13
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-01-01 18:30:04 -0600

    Added Variables and Conditional Statements
    - Now includes Numeric, Uppercase, Lowercase, and Special characters values.
    - Clicking generate password opens a prompt window for the user to set the respective variables.

--- Develop/script.js
+++ Develop/script.js
@@ -1,16 +1,48 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var lenPass = prompt ("How many characters would you like your password to be? [Min = 8, Max = 128]", 8);
+  if (lenPass <8){
+    lenPass = 8; 
+  }
+  else if (lenPass >128){
+    lenPass = 128;
+  }
+  else{}
+
+  var incNumeric = prompt ("Do you want Numeric characters? [Y/N]", "Y");
+  if (incNumeric != "Y"){
+    if (incNumeric != "N");{
+      incNumeric = "Y";
+    }
+  }
+  var incUpper = prompt ("Do you want to include Uppercase letters? [Y/N]", "Y");
+  if (incUpper != "Y"){
+    if (incUpper != "N");{
+      incUpper = "Y";
+    }
+  }
+  var incLower = prompt ("Do you want to include Lowercase letters? [Y/N]", "Y");
+  if (incLower != "Y"){
+    if (incLower != "N");{
+      incLower = "Y";
+    }
+  }
+  var incSpecial = prompt ("Do you want to include Special characters? [Y/N]", "Y");
+  if (incSpecial != "Y"){
+    if (incSpecial != "N");{
+      incSpecial = "Y";
+    }
+  }
+
+ // var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  //passwordText.value = password;
 }
 
 // Add event listener to generate button
