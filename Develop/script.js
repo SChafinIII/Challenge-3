7771e0eb5f391e56fefd3b0df0f1ce9a393d2ab3
SChafinIII <118332995+SChafinIII@users.noreply.github.com>
2023-01-02 19:26:38 -0600

    Extended Variable Additions - Update
    Added Variables for the password to generate from including Special, Uppercase, Lowercase, and Numeric characters in more defined order.

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //88 character passcode set from https://www.grc.com/ppp.htm
+  let charNumeric = "23456789";
+  let charUpper = "ABCDEFGHJKLMNOPRSTUVWXYZ";
+  let charLower = "abcdefghijkmnopqrstuvwxyz";
+  let charSpecial = "!#$%&'()*+,-./:;<=>?@[\]^_{|}~"
+
   var lenPass = prompt ("How many characters would you like your password to be? [Min = 8, Max = 128]", 8);
   if (lenPass <8){
     lenPass = 8; 
